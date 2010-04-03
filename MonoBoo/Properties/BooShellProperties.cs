e9ff78da19a650740f5d90473ae55edb7ebc3ff2
Michael Harrington <kibiz0r@gmail.com>
2010-04-02 22:17:42 -0400

    Cleaned up some things.

--- MonoBoo/Properties/BooShellProperties.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BooBinding
-{
-	public class BooShellProperties : ShellProperties
-	{
-		public override string PropertyName { get { return "BooBinding.BooShell.ShellProps"; } }
-	}
-}
